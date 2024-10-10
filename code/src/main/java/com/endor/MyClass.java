d08a6ac321222756ea62de49eccee389bea29362
shivaprakash236 <shivaprakash236@gmail.com>
2024-10-11 00:38:44 +0530

    added

---
+++ code/src/main/java/com/endor/MyClass.java
@@ -0,0 +1,15 @@
+
+package com.endor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyClass {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyClass.class);
+
+    public void method() {
+        logger.info("This is where it all begins");
+        // Do something great!
+    }
+}
---
+++ code/src/test/java/com/endor/MyClassTest.java
@@ -0,0 +1,14 @@
+
+package com.endor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MyClassTest {
+
+    @Test
+    void testMethod() {
+        assertTrue(true);
+    }
+}
