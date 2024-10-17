c7e8c427a7e1da4cea8eb350476d27858db30377
shivaprakash236 <shivaprakash236@gmail.com>
2024-10-17 11:31:13 +0530

    Addded new package

---
+++ build-config/src/main/resources/etc/main/java/com/endor/DeserialPoCconfig.java
@@ -0,0 +1,26 @@
+package com.endor;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DeserialPoC {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Provide a file with a JSON payload as argument");
+            System.exit(1);
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enableDefaultTyping();
+        try {
+            mapper.readValue(new File(args[0]), Object.class);
+        } catch (IOException e) {
+            System.err.println(e.getClass().getSimpleName() + " when deserializing: " + e.getMessage());
+        }
+
+        new com.fasterxml.jackson.databind.jsontype.impl.ClassNameIdResolver(null, null);
+    }
+}
